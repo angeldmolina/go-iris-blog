58f49b6761d122a2bb8868db205e36a0cb8259a6
angeldmolina <theangeldmolina@gmail.com>
2023-06-14 12:50:13 -0400

    added SQLite driver and GORM

--- main.go
+++ main.go
@@ -1,44 +1,59 @@
 package main
 
 import (
+	"log"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
 	"github.com/kataras/iris/v12"
 	"github.com/kataras/iris/v12/middleware/logger"
 )
 
 // Post represents a blog post.
 type Post struct {
-	ID    int    `json:"id"`
+	gorm.Model
 	Title string `json:"title"`
 	Body  string `json:"body"`
 }
 
-// Posts is an in-memory store for blog posts.
-var Posts = []Post{
-	{ID: 1, Title: "First Post", Body: "Hello, world!"},
-	{ID: 2, Title: "Second Post", Body: "Iris is awesome!"},
+// Database connection
+var db *gorm.DB
+
+// Initialize database connection
+func initDatabase() {
+	var err error
+	db, err = gorm.Open(sqlite.Open("blog.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.AutoMigrate(&Post{})
 }
 
 // getPosts returns all blog posts.
 func getPosts(ctx iris.Context) {
-	ctx.JSON(Posts)
+	var posts []Post
+	db.Find(&posts)
+	ctx.JSON(posts)
 }
 
 // getPostByID returns a specific blog post by its ID.
 func getPostByID(ctx iris.Context) {
-	id, err := ctx.Params().GetInt("id")
+	id, err := ctx.Params().GetUint("id")
 	if err != nil {
 		ctx.StatusCode(iris.StatusBadRequest)
 		return
 	}
 
-	for _, post := range Posts {
-		if post.ID == id {
-			ctx.JSON(post)
-			return
-		}
+	var post Post
+	result := db.First(&post, id)
+	if result.Error != nil {
+		ctx.StatusCode(iris.StatusNotFound)
+		return
 	}
 
-	ctx.StatusCode(iris.StatusNotFound)
+	ctx.JSON(post)
 }
 
 // createPost creates a new blog post.
@@ -50,42 +65,44 @@ func createPost(ctx iris.Context) {
 		return
 	}
 
-	// Generate a new unique ID for the post.
-	post.ID = len(Posts) + 1
-
-	Posts = append(Posts, post)
+	result := db.Create(&post)
+	if result.Error != nil {
+		ctx.StatusCode(iris.StatusInternalServerError)
+		return
+	}
 
 	ctx.StatusCode(iris.StatusCreated)
 }
 
 // deletePost deletes a blog post by its ID.
 func deletePost(ctx iris.Context) {
-	id, err := ctx.Params().GetInt("id")
+	id, err := ctx.Params().GetUint("id")
 	if err != nil {
 		ctx.StatusCode(iris.StatusBadRequest)
 		return
 	}
 
-	for i, post := range Posts {
-		if post.ID == id {
-			Posts = append(Posts[:i], Posts[i+1:]...)
-			ctx.StatusCode(iris.StatusNoContent)
-			return
-		}
+	var post Post
+	result := db.Delete(&post, id)
+	if result.Error != nil || result.RowsAffected == 0 {
+		ctx.StatusCode(iris.StatusNotFound)
+		return
 	}
 
-	ctx.StatusCode(iris.StatusNotFound)
+	ctx.StatusCode(iris.StatusNoContent)
 }
 
 func main() {
 	app := iris.New()
 	app.Use(logger.New())
 
+	initDatabase()
+
 	// Define the routes for the API.
 	app.Get("/posts", getPosts)
-	app.Get("/posts/{id:int}", getPostByID)
+	app.Get("/posts/{id:uint}", getPostByID)
 	app.Post("/posts", createPost)
-	app.Delete("/posts/{id:int}", deletePost)
+	app.Delete("/posts/{id:uint}", deletePost)
 
 	// Start the server.
 	app.Run(iris.Addr(":8080"))
