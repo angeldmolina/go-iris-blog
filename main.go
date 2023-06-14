f0d747fd9dc8a20ca03737c06b8bcb16452cb216
angeldmolina <theangeldmolina@gmail.com>
2023-06-14 08:30:56 -0400

    updated main.go

--- main.go
+++ main.go
@@ -1,56 +1,92 @@
 package main
 
-import "github.com/kataras/iris/v12"
+import (
+	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/middleware/logger"
+)
+
+// Post represents a blog post.
+type Post struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
+// Posts is an in-memory store for blog posts.
+var Posts = []Post{
+	{ID: 1, Title: "First Post", Body: "Hello, world!"},
+	{ID: 2, Title: "Second Post", Body: "Iris is awesome!"},
+}
+
+// getPosts returns all blog posts.
+func getPosts(ctx iris.Context) {
+	ctx.JSON(Posts)
+}
+
+// getPostByID returns a specific blog post by its ID.
+func getPostByID(ctx iris.Context) {
+	id, err := ctx.Params().GetInt("id")
+	if err != nil {
+		ctx.StatusCode(iris.StatusBadRequest)
+		return
+	}
+
+	for _, post := range Posts {
+		if post.ID == id {
+			ctx.JSON(post)
+			return
+		}
+	}
+
+	ctx.StatusCode(iris.StatusNotFound)
+}
+
+// createPost creates a new blog post.
+func createPost(ctx iris.Context) {
+	var post Post
+	err := ctx.ReadJSON(&post)
+	if err != nil {
+		ctx.StatusCode(iris.StatusBadRequest)
+		return
+	}
+
+	// Generate a new unique ID for the post.
+	post.ID = len(Posts) + 1
+
+	Posts = append(Posts, post)
+
+	ctx.StatusCode(iris.StatusCreated)
+}
+
+// deletePost deletes a blog post by its ID.
+func deletePost(ctx iris.Context) {
+	id, err := ctx.Params().GetInt("id")
+	if err != nil {
+		ctx.StatusCode(iris.StatusBadRequest)
+		return
+	}
+
+	for i, post := range Posts {
+		if post.ID == id {
+			Posts = append(Posts[:i], Posts[i+1:]...)
+			ctx.StatusCode(iris.StatusNoContent)
+			return
+		}
+	}
+
+	ctx.StatusCode(iris.StatusNotFound)
+}
 
 func main() {
-    app := iris.New()
+	app := iris.New()
+	app.Use(logger.New())
 
-    booksAPI := app.Party("/books")
-    {
-        booksAPI.Use(iris.Compression)
+	// Define the routes for the API.
+	app.Get("/posts", getPosts)
+	app.Get("/posts/{id:int}", getPostByID)
+	app.Post("/posts", createPost)
+	app.Delete("/posts/{id:int}", deletePost)
 
-        // GET: http://localhost:8080/books
-        booksAPI.Get("/", list)
-        // POST: http://localhost:8080/books
-        booksAPI.Post("/", create)
-    }
-
-    app.Listen(":8080")
-}
-
-// Book example.
-type Book struct {
-    Title string `json:"title"`
-}
-
-func list(ctx iris.Context) {
-    books := []Book{
-        {"Mastering Concurrency in Go"},
-        {"Go Design Patterns"},
-        {"Black Hat Go"},
-    }
-
-    ctx.JSON(books)
-    // TIP: negotiate the response between server's prioritizes
-    // and client's requirements, instead of ctx.JSON:
-    // ctx.Negotiation().JSON().MsgPack().Protobuf()
-    // ctx.Negotiate(books)
-}
-
-func create(ctx iris.Context) {
-    var b Book
-    err := ctx.ReadJSON(&b)
-    // TIP: use ctx.ReadBody(&b) to bind
-    // any type of incoming data instead.
-    if err != nil {
-        ctx.StopWithProblem(iris.StatusBadRequest, iris.NewProblem().
-            Title("Book creation failure").DetailErr(err))
-        // TIP: use ctx.StopWithError(code, err) when only
-        // plain text responses are expected on errors.
-        return
-    }
-
-    println("Received Book: " + b.Title)
-
-    ctx.StatusCode(iris.StatusCreated)
+	// Start the server.
+	app.Run(iris.Addr(":8080"))
 }
